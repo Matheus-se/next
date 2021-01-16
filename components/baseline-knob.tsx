85a3f63b882b02f29725a1871ade466f61cc481d
Matheus-se <63105495+Matheus-se@users.noreply.github.com>
2021-01-16 07:38:25 -0300

    Delete components directory

--- components/baseline-knob.tsx
+++
@@ -1,7 +0,0 @@
-export default function BaseLineKnob(props) {
-  return (
-    <div className="baseline-knob" style={{ left: props.left }}>
-      <div className="bullet"></div>
-    </div>
-  );
-}
