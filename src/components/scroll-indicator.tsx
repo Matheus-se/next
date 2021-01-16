d373940a029144a399053a0c5660f6ac682a4bf6
Matheus-se <63105495+Matheus-se@users.noreply.github.com>
2021-01-16 07:38:50 -0300

    Delete src directory

--- src/components/scroll-indicator.tsx
+++
@@ -1,9 +0,0 @@
-export default function ScrollIndicator() {
-  return (
-    <svg width="17" height="58" fill="none" className="scroll-indicator">
-      <path d="M8.065 39.333l-5.02 5.02 5.02 5.021 5.02-5.02-5.02-5.02z"></path>
-      <path d="M8.065 44.379L5.59 46.854l2.475 2.474 2.475-2.474-2.475-2.475z"></path>
-      <path d="M4.6 47.8l-1.5 1.5 5 5 5-5-1.5-1.5M8.1 4.2v30"></path>
-    </svg>
-  );
-}
